39248e00450db16b32c4fa3fdd7d043597b390d2
Antonio Salazar <antonio.salazar.devops@gmail.com>
2026-06-12 04:16:57 -0600

    update providers

--- provider.tf
+++ provider.tf
@@ -1,16 +1,8 @@
 Fterraform {
   required_providers {
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
     }
   }
 }
