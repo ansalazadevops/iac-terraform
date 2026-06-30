b3086b443623f099f7bae8561d2305088093ea3e
Antonio Salazar <antonio.salazar.devops@gmail.com>
2026-06-30 01:31:30 -0600

    Update S3 backend

---
+++ aws/ec2/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket        = "ansalaza-tf-state"
+    key           = "aws/ec2/terraform.tfstate"
+    region        = "us-east-1"
+    encrypt       = true
+    use_lockfile  = true
+}
