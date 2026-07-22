d9294f663a358ec0174b0b71768bc33d56839a28
Antonio Salazar <antonio.salazar.devops@gmail.com>
2026-07-22 10:45:07 -0600

    Deploy Zabbix

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -88,7 +88,7 @@ resource "aws_security_group" "ssh-security-group" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["${var.ssh-location}"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
