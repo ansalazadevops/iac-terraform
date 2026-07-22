38b4831ed0498c75129fdf998b85b3e49da113ca
Antonio Salazar <antonio.salazar.devops@gmail.com>
2026-07-22 03:40:27 -0600

    Deploy EKS Cluster

--- aws/eks/outputs.tf
+++ aws/eks/outputs.tf
@@ -7,12 +7,11 @@ output "aws_vpc" {
 }
 
 # Subnets
-output "aws_subnet" {
-  value = aws_subnet.public_az1.id
-}
-
-output "aws_subnet" {
-  value = aws_subnet.public_az2.id
+output "aws_subnet_ids" {
+  value = [
+    aws_subnet.public_az1.id,
+    aws_subnet.public_2.id
+  ]
 }
 
 # IGW
@@ -26,33 +25,29 @@ output "aws_route_table" {
 }
 
 # Route table association
-output "aws_route_table_association" {
-  value = aws_route_table_association.rt_association_az1.id
-}
-
-output "aws_route_table_association" {
-  value = aws_route_table_association.rt_association_az2.id
+output "aws_route_table_associations" {
+  value = [
+    aws_route_table_association.rt_association_az1.id,
+    aws_route_table_association.rt_association_az2.id
+  ]
 }
 
 # -------------------------------------
 # IAM Roles Outputs
 # -------------------------------------
-output "aws_iam_role" {
-  value = aws_iam_role.eks_cluster_role.name
-}
-
-output "aws_iam_role" {
-  value = aws_iam_role.eks_node_role.name
+output "aws_iam_roles" {
+  value = [
+    aws_iam_role.eks_cluster_role.name,
+    aws_iam_role.eks_node_role.name
+  ]
 }
 
 # -------------------------------------
 # EKS Control Plane Outputs
 # -------------------------------------
-output "eks_cluster" {
-  value = aws_eks_cluster.eks_cluster.name
+output "eks_cluster_info" {
+  value = [
+    aws_eks_cluster.eks_cluster.name,
+    aws_eks_cluster.eks_cluster.endpoint
+  ]
 }
-
-output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.eks_cluster.endpoint
-}
-
